f436226b4e1f636e594e458cbf8810be16140cdf
desenvolvainteligente <desenvolvainteligente@gmail.com>
2025-05-05 13:40:39 -0300

    Criação de rota e dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,9 +18,7 @@
         @endif
         @livewireStyles
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-
-
+    <body>
         <header>
             <div>
                 LOGO
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Dashboard;
+use App\Livewire\
+{
+    Dashboard as LivewireDashboard
+};
 
+Route::get('dashboard', LivewireDashboard::class);
 
 Route::get('/', function () {
     return view('index');
 });
-
-// Route::controller(DashboardController::class)->group(function (){
-//     Route::get('/dashboard', 'index')->name('dashboard.index');
-// });
-
-// Route::controller(PacienteController::class)->group(function() {
-//     Route::get('/paciente', 'index')->name('paciente');
-//     Route::post('/cadastro-paciente', 'store')->name('paciente.cadastro');
-//     Route::get('/cadastrar-paciente', 'create')->name('paciente.cadastrar');
-//     Route::get('/avaliar-paciente', 'avaliacao')->name('paciente.avaliacao');
-// });
-
-// Route::controller(FinanceiroController::class)->group(function () {
-//     Route::get('/financeiro', 'index');
-// });
